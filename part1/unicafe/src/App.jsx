6ac62357ffdfab5da520d21e0dc448b0eeea8a4c
Kevin <plasenciakevin007@gmail.com>
2025-03-13 14:03:35 +0000

    1.9 unicafe done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,16 +7,18 @@ const Statistics = ({good, neutral, bad}) => {
   const average = (good - bad) / all
   const positive = good / all * 100
 
+  if (all === 0) {
+    return <div>No feedback given</div>
+  }
+
   return (
     <>
-      <h1>statistics</h1>
-
-        <div>good {good}</div>
-        <div>neutral {neutral}</div>
-        <div>bad {bad}</div>
-        <div>all {all}</div>
-        <div>average {average}</div>
-        <div>positive {positive} %</div>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
     </>
   )
 }
@@ -27,7 +29,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -36,8 +37,8 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
-
     </div>
   )
 }
