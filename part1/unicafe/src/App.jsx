f5ece2762ff0b35b8f3298875a039989e9269a0d
Kevin <plasenciakevin007@gmail.com>
2025-03-13 13:26:27 +0000

    1.7 unicafe done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,10 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  // calculate statistics
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all * 100
 
   return (
     <div>
@@ -19,6 +23,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
 
     </div>
   )
